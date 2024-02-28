702909e6b733602df0974add3d51a6e241b1b164
zark <zarkalhs@gmail.com>
2024-02-28 23:32:47 +0200

    Create a fully functional simple gui for postfixie

---
+++ src/main/java/gr/aueb/dmst/postfixieCalculator/CalculatorController.java
@@ -0,0 +1,126 @@
+package gr.aueb.dmst.postfixieCalculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CalculatorController implements Initializable {
+
+    public boolean resultState = false;
+
+    @FXML
+    private TextArea numbersArea;
+    @FXML
+    private Button zeroButton;
+    @FXML
+    private Button oneButton;
+    @FXML
+    private Button twoButton;
+    @FXML
+    private Button threeButton;
+    @FXML
+    private Button fourButton;
+    @FXML
+    private Button fiveButton;
+    @FXML
+    private Button sixButton;
+    @FXML
+    private Button sevenButton;
+    @FXML
+    private Button eightButton;
+    @FXML
+    private Button nineButton;
+    @FXML
+    private Button plusButton;
+    @FXML
+    private Button minusButton;
+    @FXML
+    private Button multiplyButton;
+    @FXML
+    private Button divideButton;
+    @FXML
+    private Button removeButton;
+    @FXML
+    private Button resultButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Circle validCircle;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        numbersArea.setText("");
+
+        oneButton.setOnAction(this::addNumber);
+        twoButton.setOnAction(this::addNumber);
+        threeButton.setOnAction(this::addNumber);
+        fourButton.setOnAction(this::addNumber);
+        fiveButton.setOnAction(this::addNumber);
+        sixButton.setOnAction(this::addNumber);
+        sevenButton.setOnAction(this::addNumber);
+        eightButton.setOnAction(this::addNumber);
+        nineButton.setOnAction(this::addNumber);
+        zeroButton.setOnAction(this::addNumber);
+
+        plusButton.setOnAction(this::addOperator);
+        minusButton.setOnAction(this::addOperator);
+        multiplyButton.setOnAction(this::addOperator);
+        divideButton.setOnAction(this::addOperator);
+    }
+
+    public void addNumber(ActionEvent event) {
+        if (resultState) {
+            clearStack();
+            resultState = false;
+        }
+        Button button = (Button) event.getSource();
+        String buttonText = button.getText();
+
+        numbersArea.setText(numbersArea.getText() + " " + buttonText + " ");
+
+        Main.stack.push(new Element(Integer.parseInt(buttonText)));
+    }
+
+    public void addOperator(ActionEvent event) {
+        if (resultState) {
+            clearStack();
+            resultState = false;
+        }
+        Button button = (Button) event.getSource();
+        String buttonText = button.getText();
+
+        numbersArea.setText(numbersArea.getText() + " " + buttonText + " ");
+
+        Main.stack.push(new Element(buttonText.charAt(0)));
+    }
+
+    public void removeFromStack() {
+        if (!Main.stack.isEmpty()) {
+            numbersArea.setText(numbersArea.getText().substring(0, numbersArea.getText().length() - 2));
+            Main.stack.pop();
+        }
+    }
+
+    public void calculateResult() {
+        if (Calculator.isValidPostfix(Main.stack)) {
+            numbersArea.setText(Calculator.calculateExpression(Main.stack).toString());
+            validCircle.setFill(javafx.scene.paint.Color.GREEN);
+            resultState = true;
+        } else {
+            validCircle.setFill(javafx.scene.paint.Color.RED);
+        }
+    }
+
+    public void clearStack() {
+        Main.stack.clear();
+        numbersArea.setText("");
+    }
+}
+
+
--- src/main/java/gr/aueb/dmst/postfixieCalculator/Main.java
+++ src/main/java/gr/aueb/dmst/postfixieCalculator/Main.java
@@ -1,46 +1,11 @@
 package gr.aueb.dmst.postfixieCalculator;
 
-import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+
+    public static Stack<Element> stack = new Stack<>();
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Stack<Element> stack = new Stack<>();
-        boolean exit = false;
-        do {
-            System.out.println("You want to give a number or an operator (n/o): ");
-            String input = scanner.nextLine();
-            switch (input) {
-                case "n":
-                    System.out.println("Give a number: ");
-                    int number = scanner.nextInt();
-                    scanner.nextLine();
-                    stack.push(new Element(number));
-                    break;
-                case "o":
-                    System.out.println("Give an operator: ");
-                    char operator = scanner.nextLine().charAt(0);
-                    stack.push(new Element(operator));
-                    break;
-                case "exit":
-                    exit = true;
-            }
-        } while (!exit);
-
-        for (Element element : stack) {
-            if (element.isNumber()) {
-                System.out.println(element.getNumber());
-            } else {
-                System.out.println(element.getOperator());
-            }
-        }
-
-        if (Calculator.isValidPostfix(stack)) {
-            System.out.println("The expression is valid");
-            System.out.println("The result is: " + Calculator.calculateExpression(stack));
-        } else {
-            System.out.println("The expression is not valid");
-        }
+        GuiApplication.launch(GuiApplication.class, args);
     }
 }
